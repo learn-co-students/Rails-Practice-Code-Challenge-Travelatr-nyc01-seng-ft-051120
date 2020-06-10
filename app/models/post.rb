c4cc804be428dc074e13068505a6014a101a603c
grantkoji <grantkoji@gmail.com>
2020-06-10 15:37:14 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
+    belongs_to :blogger
+    belongs_to :destination
 
+    validates :title, presence: true
+    validates :content, length: { minimum: 100 } 
+
+    def add_like
+        self.likes += 1
+    end
 end
